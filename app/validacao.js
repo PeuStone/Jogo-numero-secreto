f96a3eb86e3e1f1bbc455aca1c5d03aa534bbfce
PeuStone <134339437+PeuStone@users.noreply.github.com>
2023-06-16 10:03:13 -0300

    Arquivos finalizados do projeto

---
+++ app/validacao.js
@@ -0,0 +1,44 @@
+function verificaValorValido(chute) {
+    const numero = +chute;
+
+    if (chuteInvalido(numero)) {
+        elementosChute.innerHTML += '<div>Valor Invalido</div>';
+        return
+    }
+
+    if (comparacaoDeValoresPermitido(numero)) {
+        elementosChute.innerHTML += `<div> Valor inválido: o numero secreto precisa estar entre ${menorValor} e ${maiorValor} </div>`;
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2> Você acertou! </h2>
+            <h3> O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class ="btn-jogar">Jogar Novamente </button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementosChute.innerHTML += `
+        <div> O número secreto é menor <i class="fa-solid fa-arrow-down"></i> </div>
+        `
+    } else {
+        elementosChute.innerHTML += `
+        <div> O número secreto é maior <i class="fa-solid fa-arrow-up"></i> </div>
+        `
+    }
+}
+
+function comparacaoDeValoresPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+function chuteInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
